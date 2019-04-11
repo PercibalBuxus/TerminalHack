e0417101a7767666cc4630af50c1ca626c2dd708
PercibalBuxus <molnar.imre98@gmail.com>
2019-04-11 12:16:19 +0200

    MemoryMapper: Row generator in progress

--- src/app/memory-mapper/memory-mapper.component.ts
+++ src/app/memory-mapper/memory-mapper.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Row } from '../Row';
 import { HexNumber } from '../HexNumber';
 import { Character } from '../Character';
+import { CrackerService } from '../cracker.service';
 
 @Component({
   selector: 'app-memory-mapper',
@@ -11,15 +12,32 @@ import { Character } from '../Character';
 export class MemoryMapperComponent implements OnInit {
 
   private attemptsRemained: number;
-  public rowNumbers: String[];
+  public firstRowNumbers: String[];
+  public secondRowNumbers: String[];
+  public firstRows: Row[];
+  public secondRows: Row[];
 
-  constructor() { 
+  constructor(
+    private cracker: CrackerService
+  ) { 
     this.attemptsRemained = 4;
     let start = Math.floor(Math.random()*1000000);
     console.log(start);
-    for(var i = 0; i < 32; i++){
+    this.firstRowNumbers = new Array<String>(16);
+    this.secondRowNumbers = new Array<String>(16);
+    this.firstRows = new Array<Row>(16);
+    this.secondRows = new Array<Row>(16);
+
+    for(var i = 0; i < 16; i++){
       let hexnum = new HexNumber(start)
-      this.rowNumbers.push(hexnum.toString());
+      this.firstRowNumbers.push(hexnum.toString());
+      this.firstRows.push(new Row(hexnum.decimalValue,this.cracker.randomString(12)));
+      start += 12;
+    }
+    for(var i = 0; i < 16; i++){
+      let hexnum = new HexNumber(start)
+      this.secondRowNumbers.push(hexnum.toString());
+      this.secondRows.push(new Row(hexnum.decimalValue,new String(this.cracker.randomString(12))));
       start += 12;
     }
   }
